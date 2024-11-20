186bf4b96ccc268bb9987b6a0fb7c91ec127d4c9
Cherrelle Scott <cherrelle.scott@gmail.com>
2024-11-20 17:21:33 -0500

    Update signalwire_magic8ball.rb
    update code w/ SignalWire information

--- ruby/signalwire_magic8ball.rb
+++ ruby/signalwire_magic8ball.rb
@@ -1,17 +1,17 @@
 #use SignalWire to text myself
 require 'rubygems'
-require 'twilio-ruby'
+require 'signalwire'
 
-#connect to Twilio API using account SID and auth token
-account_sid = ""
-auth_token = ""
+#connect to SignalWire API using account SID and auth token
+account_sid = "b241c29e-066b-493f-8a73-037e48c40bbf"
+auth_token = "PT81e4538d1b10b73efd98f92a0ea43676cb1122c5ade3150a"
 
 @client = Signalwire::REST::Client.new(account_sid, auth_token)
 
 #text yourself a message
 message = @client.account.messages.create(
-	:from => "+your SignalWire number",
-	:to => "+your own number",
+	:from => "+12012772800",
+	:to => "+19105464691",
 	:body => "Haaaii!"
 )
 
